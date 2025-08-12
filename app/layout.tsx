f63ea5882e440f4501b3063bcdf57746bb0ff3b4
Gabriel Ramirez <gabrielrs874@gmail.com>
2025-08-12 12:48:16 -0400

    cambio de layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script"; // ✅ Importa Script
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
